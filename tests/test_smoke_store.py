0d4ee1c174c54f07c9f16b9621474e036321d6b3
Aleksandr <Jungleman150@gmail.com>
2024-04-07 16:59:35 +0300

    Add  action

--- tests/test_smoke_store.py
+++ tests/test_smoke_store.py
@@ -16,6 +16,7 @@ from pages.basket_page import Basket_page
 def test_smoke(start_and_finish):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
+    options.add_argument("--headless")
     driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
     print("Start Test 1!")
